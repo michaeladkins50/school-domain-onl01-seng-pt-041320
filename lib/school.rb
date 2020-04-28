5444cddbe3fb16e90a83dc1ce51e11ca9b96669e
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-28 20:08:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,17 @@ class School
   def roster
     @roster
   end
-  
+    def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
   
 end
